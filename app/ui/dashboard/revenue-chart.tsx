2c2018c55ca4bf0ad0c829d87b7b6466a0bc294b
Robin-Kurian <dev.robinkurian@gmail.com>
2024-12-18 23:12:06 +0530

    Update UI components to use teal color scheme for improved visual consistency
    - Changed background colors from blue to teal in various components including the main page, error page, invoice edit not found page, button component, revenue chart, and invoice buttons.
    - Updated hover states to match the new teal color scheme for a cohesive user experience.

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -2,7 +2,7 @@ import { generateYAxis } from "@/app/lib/utils";
 import { CalendarIcon } from "@heroicons/react/24/outline";
 import { lusitana } from "@/app/ui/fonts";
 import { Revenue } from "@/app/lib/definitions";
-import { fetchRevenue } from '@/app/lib/data';
+import { fetchRevenue } from "@/app/lib/data";
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -42,7 +42,7 @@ export default async function RevenueChart() {
           {revenue.map((month) => (
             <div key={month.month} className="flex flex-col items-center gap-2">
               <div
-                className="w-full rounded-md bg-blue-300"
+                className="w-full rounded-md bg-teal-300"
                 style={{
                   height: `${(chartHeight / topLabel) * month.revenue}px`,
                 }}
